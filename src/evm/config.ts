dd5345ddfd7b77d1fb6e0c317b903c7c13c9f030
Jaydearcadian <desolatejayson@gmail.com>
2026-07-31 09:19:30 +0100

    fix: target corrected Sepolia escrow

--- src/evm/config.ts
+++ src/evm/config.ts
@@ -5,7 +5,7 @@ export const PACTPAY_CHAIN = sepolia;
 export const PACTPAY_CHAIN_ID_HEX = '0xaa36a7' as const;
 
 export const PUSDC_ADDRESS = '0x543b3e6038D10d92648D529200C432f8FdAB63e3' satisfies Address;
-export const PACTPAY_ESCROW_ADDRESS = '0x8dd98bDFa3914Ef5F213dFd92679F587B88D51B6' satisfies Address;
+export const PACTPAY_ESCROW_ADDRESS = '0x3F7fc504AC9096C2fFb28ef32D85d08B1034f3a6' satisfies Address;
 
 export const PUSDC_DECIMALS = 6;
 export const MIN_REVIEW_PERIOD_SECONDS = 60 * 60;
