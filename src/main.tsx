b65259fefd4e36290970933ced134c37d34d1f28
Jaydearcadian <desolatejayson@gmail.com>
2026-07-31 19:04:50 +0100

    fix: route response links through safe importer

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
+import { ResponseApp } from './app/ResponseApp';
 import './styles.css';
 
+function RootRouter() {
+  const [hash, setHash] = useState(window.location.hash);
+
+  useEffect(() => {
+    const onHashChange = () => setHash(window.location.hash);
+    window.addEventListener('hashchange', onHashChange);
+    return () => window.removeEventListener('hashchange', onHashChange);
+  }, []);
+
+  return hash.startsWith('#/response/') ? <ResponseApp /> : <App />;
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode><RootRouter /></React.StrictMode>,
 );
